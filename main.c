ba290c90f7b01d9cc01a4ab30eaca5c5338807ce
PraneethKur <48721244+PraneethKur@users.noreply.github.com>
2019-04-04 15:35:43 +1100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+//Variables//
+
+char letter ='a';//letter a=65
+int Rotat;//Rotation Key
+
+
+
+printf ("enter Rotation amount")
+
+
+
+
+printf("Enter Message");
+scanf ("%c", &c); // &c, not a string
+//Rotaional Cipher 
+//Decrypt
+//Encrypt
+//Depends on whether a basic word is found in the sentence
+
+}
+return 0;
+}
+
